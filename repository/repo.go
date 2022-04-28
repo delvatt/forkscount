39a8a0f60dd06bd9ab26fbbac4ca42e99c546849
Elvis Attro <attroelvis@gmail.com>
2022-04-28 16:20:21 +0000

    add inMemoryRepository implementation.

--- repository/repo.go
+++ repository/repo.go
@@ -13,3 +13,43 @@ type Project struct {
 type Repository interface {
 	Fetch(context.Context, int) ([]Project, error)
 }
+
+type inMemoryRepository struct {
+	fakeProjects []Project
+}
+
+func NewInMemoryRepository() *inMemoryRepository {
+	ir := inMemoryRepository{}
+	ir.fakeProjects = []Project{
+		{Name: "Boner project", ForksCount: 0},
+		{Name: "grup", ForksCount: 0},
+		{Name: "easy", ForksCount: 2},
+		{Name: "slothbeast", ForksCount: 4},
+		{Name: "sspssptest", ForksCount: 0},
+		{Name: "hcs_utils", ForksCount: 1},
+		{Name: "K", ForksCount: 1},
+		{Name: "Heroes of Wesnoth", ForksCount: 5},
+		{Name: "Leiningen", ForksCount: 1},
+		{Name: "TearDownWalls", ForksCount: 5},
+	}
+
+	return &ir
+}
+
+func (ir *inMemoryRepository) Fetch(ctx context.Context, n int) ([]Project, error) {
+	repoChan := make(chan []Project)
+
+	go func() {
+		repoChan <- ir.fakeProjects
+	}()
+
+	var err error
+	latestProjects := []Project{}
+
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+	case latestProjects = <-repoChan:
+	}
+	return latestProjects, err
+}
